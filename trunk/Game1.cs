f7814e04fa5af9295fc327ebf150407d93c680fd
punchi10@gmail.com <punchi10@gmail.com@95c350b4-299c-11df-8871-772815e25991>
2010-05-07 02:45:25 +0000

    - Pantalla de seleccion de personajes, prototipo - Seleccion de personaje y se puede jugar con él

--- trunk/Game1.cs
+++ trunk/Game1.cs
@@ -11,7 +11,6 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
 
-
 namespace TarreeGame
 {
     /// <summary>
@@ -27,10 +26,15 @@ namespace TarreeGame
         Bala bala;
         Vector2 inputplayer;
         Vector2 hudfrags=new Vector2(10,10);
-        Pantallas menu_principal;
+        Pantalla menu_principal;
         string eleccionplayer;
+        List<Texture2D> fotoplayer = new List<Texture2D>(3);
         int indice_eleccionplayer = 0;
         string pantalla = "EleccionPlayer";
+        string texto_player;
+        
+        bool bloqueo_izq = false;
+        bool bloqueo_der = false;
 
         #region Multiplayer
 
@@ -75,15 +79,21 @@ namespace TarreeGame
         /// </summary>
         protected override void LoadContent()
         {
+            
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             spritefont = Content.Load<SpriteFont>(@"font\font");
+            
+            fotoplayer.Add(Content.Load<Texture2D>(@"Texture\Juba"));
+            fotoplayer.Add(Content.Load<Texture2D>(@"Texture\Lito"));
+            fotoplayer.Add(Content.Load<Texture2D>(@"Texture\Mava"));
+            fotoplayer.Add(Content.Load<Texture2D>(@"Texture\Punchi"));
 
-            player = new Jugador(Content,eleccionplayer);
+            //player = new Jugador(Content, "Mava");
             Punchi = new Jugador(Content, "Punchi");
             bala = new Bala(Content);
 
-            menu_principal = new Pantallas(5);
+            menu_principal = new Pantalla(5);
             menu_principal.anadir(0, "Presiona F1 para comenzar po shoro! (Single Player)");
             menu_principal.anadir(1, "Presiona F2 para volver esta ventana");
             menu_principal.anadir(2, "Presiona F3 para cambiar personaje");
@@ -118,12 +128,17 @@ namespace TarreeGame
             // TODO: Add your update logic here
             if (pantalla == "EleccionPlayer")
             {
-                UpdateEleccionPlayer(keystate);
+                UpdateEleccionPlayer(keystate, gameTime);
             }
             
             if (pantalla == "Juego")
             {
                 CheckMovmentPlayer(keystate);
+                /*if (!player.en_juego)
+                {
+                    player = new Jugador(Content, eleccionplayer);
+                    player.en_juego = true;
+                }*/
                 bala.disparar(gameTime, mouse, player);
                 ImpactoPlayer();
             }
@@ -153,7 +168,6 @@ namespace TarreeGame
             // TODO: Add your drawing code here
             //spriteBatch.Begin();
 
-            
 
             if (pantalla == "EleccionPlayer")
             {
@@ -166,7 +180,15 @@ namespace TarreeGame
             }
             else 
             {
-                DrawGameplay(); 
+            Console.WriteLine(eleccionplayer);
+            spriteBatch.Begin();
+            player.Draw(spriteBatch,spritefont);
+            Punchi.Draw(spriteBatch,spritefont);
+            if (bala.activo == true) { bala.Draw(spriteBatch); }
+            //spriteBatch.DrawString(spritefont, "frags: " + player.frags.ToString(), hudfrags, Color.White);
+            //spriteBatch.DrawString(spritefont, "frags: " + player.frags.ToString(), hudfrags+new Vector2(1,1), Color.YellowGreen);
+            spriteBatch.End();
+
             }
 
             base.Draw(gameTime);
@@ -178,54 +200,64 @@ namespace TarreeGame
         /// 
         void DrawEleccionPlayer() 
         {
+
             
-
-            /*spriteBatch.DrawString(spritefont,"Presiona F1 para comenzar po shoro! (Single Player) "
-                                               + "\nPresiona F2 para volver esta ventana"
-                                               + "\nPresiona F3 para cambiar personaje"
-                                               + "\nPresiona F4 para iniciar como SERVIDOR"
-                                               + "\nPresiona F5 para iniciar como CLIENTE", new Vector2(160, 160), Color.Black);
-            spriteBatch.DrawString(spritefont, "Presiona F1 para comenzar po shoro! "
-                                               +"\nPresiona F2 para volver esta ventana"
-                                               +"\nPresiona F3 para cambiar personaje", new Vector2(161, 161), Color.Red);*/
-
             switch (indice_eleccionplayer)
             {
                 case 0:
                     eleccionplayer = "Juba";
+                    texto_player = "Jugador gay, dispara tulas. Un maraco de seleccion.";
                     break;
                 case 1:
                     eleccionplayer = "Lito";
+                    texto_player = "Cagamos. Jugai con este wn y lagazo o reseteo\ninmediato.";
                     break;
                 case 2:
                     eleccionplayer = "Mava";
+                    texto_player = "Mala cuea maxima, cualquier wea mala que pase\nen el mundo, es culpa de Mava.";
                     break;
                 case 3:
                     eleccionplayer = "Punchi";
+                    texto_player = "Macabeismo es su definicion, 99% de su vida\ncon pololi.";
                     break;
             }
 
             spriteBatch.DrawString(spritefont, "Jugador: " + eleccionplayer, new Vector2(160, 330), Color.Black);
             spriteBatch.DrawString(spritefont, "Jugador: " + eleccionplayer, new Vector2(161, 331), Color.Red);
+            spriteBatch.Draw(fotoplayer[indice_eleccionplayer], new Rectangle(180, 360, 80, 100), Color.White);
+            spriteBatch.DrawString(spritefont, texto_player, new Vector2(280, 360), Color.Red);
+            spriteBatch.DrawString(spritefont, "Temporal: " + "", new Vector2(161, 40), Color.Red);
             
         }
 
-        void UpdateEleccionPlayer(KeyboardState ksstate)
+        void UpdateEleccionPlayer(KeyboardState ksstate, GameTime gametime)
         {
+
+            if (ksstate.IsKeyUp(Keys.Left))
+            {
+                bloqueo_izq = true;
+            }
+
+            if (ksstate.IsKeyUp(Keys.Right))
+            {
+                bloqueo_der = true;
+            }
 
             if (ksstate.IsKeyDown(Keys.Left))
             {
-                if (indice_eleccionplayer > 0)
+                if (bloqueo_izq == true && indice_eleccionplayer > 0)
                 {
                     indice_eleccionplayer--;
+                    bloqueo_izq = false;
                 }
             }
 
             if (ksstate.IsKeyDown(Keys.Right))
             {
-                if (indice_eleccionplayer < 4)
+                if (bloqueo_der == true && indice_eleccionplayer < 3)
                 {
                     indice_eleccionplayer++;
+                    bloqueo_der = false;
                 }
             }
             
@@ -237,18 +269,7 @@ namespace TarreeGame
 
         }
 
-        void DrawGameplay() 
-        {
-            spriteBatch.Begin();
-
-            player.Draw(spriteBatch,spritefont);
-            Punchi.Draw(spriteBatch,spritefont);
-            if (bala.activo == true) { bala.Draw(spriteBatch); }
-            spriteBatch.DrawString(spritefont, "frags: " + player.frags.ToString(), hudfrags, Color.White);
-            spriteBatch.DrawString(spritefont, "frags: " + player.frags.ToString(), hudfrags+new Vector2(1,1), Color.YellowGreen);
-            spriteBatch.End();
-
-        }
+        
         public void ImpactoPlayer() 
         {
             if (Punchi.ImpactoBala(bala))
@@ -268,8 +289,13 @@ namespace TarreeGame
         }
         public void UpdateMenu(KeyboardState ksstate) 
         {
-            if (ksstate.IsKeyDown(Keys.F1)) { pantalla = "Juego"; }
+            if (ksstate.IsKeyDown(Keys.F1))
+            {
+                player = new Jugador(Content, eleccionplayer);
+                pantalla = "Juego";
+            }
             if (ksstate.IsKeyDown(Keys.F2)) { pantalla = "EleccionPlayer"; }
+
 //            if (ksstate.IsKeyDown(Keys.F3)&& pantalla == "EleccionPlayer")
 //            {
 //                if (eleccionplayer == "Mava") { eleccionplayer = "Punchi"; }
